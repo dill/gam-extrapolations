0b53d53b94f405dc5a1cda13c34961a6698bb6fb
David Lawrence Miller <github@ninepointeightone.net>
2016-05-16 11:10:35 -0400

    minor typo, put all plots on same y axes

--- 1d_extrapolation_code.R
+++ 1d_extrapolation_code.R
@@ -62,10 +62,11 @@ fv6 <- predict(b6,pd,se=TRUE)
 
 par(mfrow=c(2,3))
 n_sims = 50
+ylim <- c(-125,125)
 ## black order 2 penalty...
 ll <- fv1$fit-fv1$se.fit*2;ul <- fv1$fit+fv1$se.fit*2
 post_sim = sim_post_curves(b1,pd,n_sims)
-plot(pd$x,fv1$fit,ylim=range(c(ul,ll)),type="l",xlab="x",ylab="y",
+plot(pd$x,fv1$fit,ylim=ylim,type="l",xlab="x",ylab="y",
      main  ="gam(y~s(x,m=2,k=k))",lwd=2)
 lines(pd$x,ll,lty=2,lwd=2);lines(pd$x,ul,lty=2,lwd=2)
 matlines(pd$x, post_sim, col=alpha("black",0.25),lty=1)
@@ -74,7 +75,7 @@ points(x,y,pch=19,cex=.4)
 ## blue order 2 penalty + pseudodata...
 ll <- fv4$fit-fv4$se.fit*2;ul <- fv4$fit+fv4$se.fit*2
 post_sim = sim_post_curves(b4,pd,n_sims)
-plot(pd$x,fv4$fit,type="l",ylim=range(c(ul,ll)),xlab="x",ylab="y",
+plot(pd$x,fv4$fit,type="l",ylim=ylim,xlab="x",ylab="y",
      main  ="gam(y~s(x,m=2,k=k*2),\nweights=w, data=dat_psuedo)",
      col=4,lwd=2)
 lines(pd$x,ll,lty=2,col=4,lwd=2);lines(pd$x,ul,lty=2,col=4,lwd=2)
@@ -84,7 +85,7 @@ points(x,y,pch=19,cex=.4)
 ## light blue order 2 penalty + extended knots...
 ll <- fv6$fit-fv6$se.fit*2;ul <- fv6$fit+fv6$se.fit*2
 post_sim = sim_post_curves(b6,pd,n_sims)
-plot(pd$x,fv6$fit,type="l",ylim=range(c(ul,ll)),xlab="x",ylab="y",
+plot(pd$x,fv6$fit,type="l",ylim=ylim,xlab="x",ylab="y",
      main  ="gam(y~s(x,m=2,k=k*2),\nknots= seq(min_exp,max_exp,length=k*2)",
      col="lightblue2",lwd=2)
 lines(pd$x,ll,lty=2,col="lightblue2",lwd=2);lines(pd$x,ul,lty=2,col="lightblue2",lwd=2)
@@ -94,7 +95,7 @@ points(x,y,pch=19,cex=.4)
 ## red order 1 penalty + linear effect...
 ll <- fv2$fit-fv2$se.fit*2;ul <- fv2$fit+fv2$se.fit*2
 post_sim = sim_post_curves(b2,pd,n_sims)
-plot(pd$x,fv2$fit,ylim=range(c(ul,ll)),type="l",xlab="x",ylab="y",
+plot(pd$x,fv2$fit,ylim=ylim,type="l",xlab="x",ylab="y",
      main  ="gam(y~s(x,m=1,k=k)+x)",col=2,lwd=2)
 lines(pd$x,ll,lty=2,col=2,lwd=2);lines(pd$x,ul,lty=2,
                                        col=2,lwd=2)
@@ -104,7 +105,7 @@ points(x,y,pch=19,cex=.4)
 ## dark green order 1 penalty + pseudodata...
 ll <- fv3$fit-fv3$se.fit*2;ul <- fv3$fit+fv3$se.fit*2
 post_sim = sim_post_curves(b3,pd,n_sims)
-plot(pd$x,fv3$fit,type="l",ylim=range(c(ul,ll)),xlab="x",ylab="y",
+plot(pd$x,fv3$fit,type="l",ylim=ylim,xlab="x",ylab="y",
      main  ="gam(y~s(x,m=1,k=k*2),\nweights=w,data=dat_psuedo)",
      col="green4",lwd=2)
 lines(pd$x,ll,lty=2,col="green4",lwd=2);lines(pd$x,ul,lty=2,col="green4",lwd=2)
@@ -115,7 +116,7 @@ points(x,y,pch=19,cex=.4)
 ## light green order 1 penalty + extended knots...
 ll <- fv5$fit-fv5$se.fit*2;ul <- fv5$fit+fv5$se.fit*2
 post_sim = sim_post_curves(b5,pd,n_sims)
-plot(pd$x,fv5$fit,type="l",ylim=range(c(ul,ll)),xlab="x",ylab="y",
+plot(pd$x,fv5$fit,type="l",ylim=ylim,xlab="x",ylab="y",
      main  ="gam(y~s(x,m=1,k=k*2),\nknots= seq(min_exp,max_exp,length=k*2)",
      col=3,lwd=2)
 lines(pd$x,ll,lty=2,col=3,lwd=2);lines(pd$x,ul,lty=2,col=3,lwd=2)
@@ -124,7 +125,7 @@ points(x,y,pch=19,cex=.4)
 
 #testing computation time for each extrapolation solution (assuming m=1):
 linear_trend_time = microbenchmark(gam(y~s(x,m=1,k=k)+x,method="REML"),times = 25,unit = "ms")
-psuedodata_time  = microbenchmark(gam(y~s(x,m=1,k=k*2),weights=w,
+pseudodata_time  = microbenchmark(gam(y~s(x,m=1,k=k*2),weights=w,
                                       data=dat_psuedo,method="REML"),
                                   times = 25,unit = "ms")
 ext_knots_time  = microbenchmark(gam(y~s(x,m=1,k=k*2),method="REML",
