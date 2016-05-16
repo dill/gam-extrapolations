455592496b8372fc926025b0038be2d7c9b2999e
David Lawrence Miller <github@ninepointeightone.net>
2016-05-16 11:26:04 -0400

    slightly better unified ylims

--- 1d_extrapolation_code.R
+++ 1d_extrapolation_code.R
@@ -28,10 +28,10 @@ ff <- function(x2, func=1) {
 n <- 200
 n_psuedo = 10
 k = 50
-func_type = 2
+func_type = 4
 sigma = 1
 #maximum and minimum ranges to extrapolate over
-min_exp = -1 
+min_exp = -1
 max_exp = 2
 x <- runif(n,0,1)
 y <- rnorm(n,ff(x,func = func_type),sigma)
@@ -42,14 +42,14 @@ dat_psuedo = data.frame(x=  c(seq(min_exp,0,length=n_psuedo),
 
 pd <- data.frame(x=seq(min_exp,max_exp,length=200))
 
-b1 <- gam(y~s(x,m=2,k=k),method="REML")
-b2 <- gam(y~s(x,m=1,k=k)+x,method="REML")
-b3 <- gam(y~s(x,m=1,k=k*2),weights=w,
-          data=dat_psuedo,method="REML")
-b4 <- gam(y~s(x,m=2,k=k*2),weights=w, 
-          data=dat_psuedo,method="REML")
-b5 <- gam(y~s(x,m=1,k=k*2),method="REML",knots= list(x=seq(min_exp,max_exp,length=k*2)))
-b6 <- gam(y~s(x,m=2,k=k*2),method="REML",knots= list(x=seq(min_exp,max_exp,length=k*2)))
+b1 <- gam(y~s(x, m=2, k=k), method="REML")
+b2 <- gam(y~s(x, m=1, k=k)+x, method="REML")
+b3 <- gam(y~s(x, m=1, k=k*2), weights=w,
+          data=dat_psuedo, method="REML")
+b4 <- gam(y~s(x, m=2, k=k*2), weights=w,
+          data=dat_psuedo, method="REML")
+b5 <- gam(y~s(x, m=1, k=k*2), method="REML", knots= list(x=seq(min_exp, max_exp, length=k*2)))
+b6 <- gam(y~s(x, m=2, k=k*2), method="REML", knots= list(x=seq(min_exp, max_exp, length=k*2)))
 
 
 fv1 <- predict(b1,pd,se=TRUE)
@@ -62,7 +62,14 @@ fv6 <- predict(b6,pd,se=TRUE)
 
 par(mfrow=c(2,3))
 n_sims = 50
-ylim <- c(-125,125)
+
+# set same ylims across plots
+if(func_type != 4){
+  ylim <- c(-125,125)
+}else{
+  ylim <- c(-50, 50)
+}
+
 ## black order 2 penalty...
 ll <- fv1$fit-fv1$se.fit*2;ul <- fv1$fit+fv1$se.fit*2
 post_sim = sim_post_curves(b1,pd,n_sims)
@@ -123,13 +130,13 @@ lines(pd$x,ll,lty=2,col=3,lwd=2);lines(pd$x,ul,lty=2,col=3,lwd=2)
 matlines(pd$x, post_sim, col=alpha(3,0.25),lty=1)
 points(x,y,pch=19,cex=.4)
 
-#testing computation time for each extrapolation solution (assuming m=1):
-linear_trend_time = microbenchmark(gam(y~s(x,m=1,k=k)+x,method="REML"),times = 25,unit = "ms")
-pseudodata_time  = microbenchmark(gam(y~s(x,m=1,k=k*2),weights=w,
-                                      data=dat_psuedo,method="REML"),
-                                  times = 25,unit = "ms")
-ext_knots_time  = microbenchmark(gam(y~s(x,m=1,k=k*2),method="REML",
-                                     knots= list(x=seq(min_exp,max_exp,length=k*2))),
-                                  times = 25,unit = "ms")
+##testing computation time for each extrapolation solution (assuming m=1):
+#linear_trend_time = microbenchmark(gam(y~s(x,m=1,k=k)+x,method="REML"),times = 25,unit = "ms")
+#pseudodata_time  = microbenchmark(gam(y~s(x,m=1,k=k*2),weights=w,
+#                                      data=dat_psuedo,method="REML"),
+#                                  times = 25,unit = "ms")
+#ext_knots_time  = microbenchmark(gam(y~s(x,m=1,k=k*2),method="REML",
+#                                     knots= list(x=seq(min_exp,max_exp,length=k*2))),
+#                                  times = 25,unit = "ms")
 
 
